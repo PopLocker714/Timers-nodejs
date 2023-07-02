d0ce4d81167caa76ede6c37855b321ae39b3b23a
Ilya <jonirootman714@gmail.com>
2023-07-02 13:11:15 +0300

    fix bag

--- 03_hello/date.js
+++ 03_hello/date.js
@@ -1,7 +1,9 @@
 const currentDateTime = () => {
   const date = new Date();
   return {
-    date: `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`,
+    date: `${date.getFullYear()}-${date.getMonth() + 1 <= 10 ? `0${date.getMonth() + 1}` : `${date.getMonth() + 1}`}-${
+      date.getDate() + 1 <= 10 ? `0${date.getDate() + 1}` : `${date.getDate() + 1}`
+    }`,
     time: Intl.DateTimeFormat("ru", { hour: "numeric", second: "numeric", minute: "numeric" }).format(date),
   };
 };
