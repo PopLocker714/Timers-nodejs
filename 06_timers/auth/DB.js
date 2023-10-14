7717bb7e97bc270718036e4d7b7c42c9a4e2af85
jojo <jojo@pop-os.localdomain>
2023-10-14 14:13:15 +0300

    fix teimer

--- 06_timers/auth/DB.js
+++ 06_timers/auth/DB.js
@@ -7,22 +7,26 @@ const DB = {
       username: "admin",
       password: "5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5", // 12345
       timers: [
-        {
-          start: Date.now() - 5000,
-          end: Date.now() - 3000,
-          duration: 2000,
-          description: "Timer 0",
-          isActive: false,
-          id: nanoid(),
-        },
-        {
-          start: Date.now() - 6000,
-          end: Date.now() - 4000,
-          duration: 3000,
-          description: "Timer 1",
-          isActive: false,
-          id: nanoid(),
-        },
+        [
+          {
+            start: Date.now() - 5000,
+            end: Date.now() - 3000,
+            duration: 2000,
+            description: "Timer 0",
+            isActive: false,
+            id: nanoid(),
+          },
+        ],
+        [
+          {
+            start: Date.now() - 6000,
+            end: Date.now() - 4000,
+            duration: 3000,
+            description: "Timer 1",
+            isActive: false,
+            id: nanoid(),
+          },
+        ],
       ],
     },
   ],
--- 06_timers/auth/modules/timers.js
+++ 06_timers/auth/modules/timers.js
@@ -7,11 +7,8 @@ const router = express.Router();
 function createTimerInterval(timer) {
   const interval = setInterval(() => {
     timer.progress += 1000;
-    if (timer.progress >= timer.duration) {
-      timer.isActive = false;
-      clearInterval(interval);
-    }
   }, 1000);
+  return interval;
 }
 
 router.get("/", auth(), (req, res) => {
@@ -19,40 +16,43 @@ router.get("/", auth(), (req, res) => {
     return res.sendStatus(401);
   }
   if (req.query.isActive === "true") {
-    const activeTimers = req.timers.filter((timer) => timer.isActive);
-    return res.json(activeTimers);
+    const activeTimers = req.timers.filter((timer) => timer[0].isActive);
+    return res.json(activeTimers.map((item) => item[0]));
   }
-  const oldTimers = req.timers.filter((timer) => !timer.isActive);
-  res.json(oldTimers);
+  const oldTimers = req.timers.filter((timer) => !timer[0].isActive);
+  res.json(oldTimers.map((item) => item[0]));
 });
 
 router.post("/", auth(), (req, res) => {
   if (!req.user) {
     return res.sendStatus(401);
   }
-
   const timer = {
-    start: Date.now() - 21000,
-    end: Date.now() - 11000,
-    duration: 10000,
+    start: Date.now(),
     progress: 0,
     id: nanoid(),
     isActive: true,
     ...req.body,
   };
-  req.timers.push(timer);
-  createTimerInterval(req.timers[req.timers.length - 1]);
-  res.json(timer);
+  req.timers.push([timer, createTimerInterval(timer)]);
+  res.json(req.timers[req.timers.length - 1][0]);
 });
 
 router.post("/:id/stop", auth(), (req, res) => {
   if (!req.user) {
     return res.sendStatus(401);
   }
-  const timerIndex = req.timers.findIndex((timer) => timer.id === req.params.id);
+  const timerIndex = req.timers.findIndex((timer) => timer[0].id === req.params.id);
   if (timerIndex !== -1) {
-    req.timers[timerIndex].isActive = false;
-    res.json(req.timers[timerIndex]);
+    const timer = req.timers[timerIndex];
+    const { start, progress, isActive } = timer[0];
+    timer[0].isActive = !isActive;
+    clearTimeout(timer[1]);
+    delete timer[1];
+    timer.pop();
+    timer[0].end = start + progress;
+    timer[0].duration = progress;
+    res.json(timer[0]);
   } else {
     res.status(404).json({ error: "Таймер не найден" });
   }
